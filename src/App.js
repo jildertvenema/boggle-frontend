dc9ea3a10c971f5821903db60f1a62baded02017
Jildert Venema <jildert.venema@hotmail.com>
2019-03-23 15:36:36 +0100

    init boggle frontend

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Websocket from 'react-websocket';
 
-class App extends Component {
+class ProductDetail extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      rooms: [],
+      room: {}
+    };
+  }
+
+  handleData = (data) => {
+    let result = JSON.parse(data);
+    console.log(result)
+    this.setState(result);
+  }
+
+  sendMessage = (action, options) => {
+    this.refWebSocket.sendMessage(JSON.stringify({ action, options }));
+  }
+
+  joinRoom = roomName => {
+    this.sendMessage('joinRoom', { roomName })
+  }
+
   render() {
+    const { room, rooms } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Websocket url='ws://localhost:3200'
+            ref={Websocket => {
+              this.refWebSocket = Websocket;
+            }}
+            onMessage={this.handleData}/>
+            {
+              room.roomName && `${room.roomName} (${room.count})`
+            }
+            {
+             rooms.map(room => <div key={room.roomName}><button onClick={() => this.joinRoom(room.roomName)}>{`${room.roomName} (${room.count})`}</button></div>)
+            }
       </div>
     );
   }
 }
 
-export default App;
+export default ProductDetail;
